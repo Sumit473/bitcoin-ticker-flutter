ab8377b8596225b17428e2f129ee7b257a0d05b6
Sumit Garg <gargs473@gmail.com>
2020-10-04 16:43:44 +0530

    Successful conversion of BTC into selected currency

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,4 +1,7 @@
-const List<String> currenciesList = [
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+List<String> currenciesList = [
   'AUD',
   'BRL',
   'CAD',
@@ -28,4 +31,21 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apiKey = '91781ECF-2F67-48C1-9101-93CB6CF9B482';
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    http.Response response = await http.get('$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey');
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    }
+    else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -3,6 +3,10 @@ import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
 import 'dart:io' show Platform;
 
+import 'coin_data.dart';
+import 'coin_data.dart';
+import 'coin_data.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
@@ -10,6 +14,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = currenciesList[0];
+  String bitcoinValue = '?';
 
   DropdownButton<String> androidDropdown() {
     List< DropdownMenuItem<String>> dropdownItems = [];
@@ -33,6 +38,7 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getData();
         });
       },
     );
@@ -56,10 +62,29 @@ class _PriceScreenState extends State<PriceScreen> {
       backgroundColor: Colors.lightBlue,
       itemExtent: 32.0,
       onSelectedItemChanged: (selectedIndex) {
-        print(selectedIndex);
+        selectedCurrency = currenciesList[selectedIndex];
+        getData();
       },
       children: pickerItems,
     );
+  }
+
+  void getData() async{
+    try{
+      double data = await CoinData().getCoinData(selectedCurrency);
+      setState(() {
+        bitcoinValue = data.toStringAsFixed(0);
+      });
+    }
+    catch(e){
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
   }
 
   @override
@@ -83,7 +108,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinValue $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
